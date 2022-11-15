9663866f1737389ce2c63e1dc3fbfaffcbe3d51b
v111222 <v111222@notmail.com>
2022-11-16 01:22:50 +0500

    first commit

---
+++ src/main/java/ru/mefodovsky/test_rest_service/controller/MyController.java
@@ -0,0 +1,29 @@
+package ru.mefodovsky.test_rest_service.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import ru.mefodovsky.test_rest_service.model.Request;
+import ru.mefodovsky.test_rest_service.model.Response;
+
+@RestController
+public class MyController {
+
+    @PostMapping(value = "/feedback")
+    public ResponseEntity<Response> feedback(@RequestBody Request request) {
+
+        Response response = Response.builder()
+                .uid(request.getUid())
+                .operationUid(request.getOperationUid())
+                .systemTime(request.getSystemTime())
+                .code("success")
+                .errorCode("")
+                .errorMessage("")
+                .build();
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+}
