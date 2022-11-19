ae4c799f8de0d9cb6e8234d008aa71364b851207
v111222 <v111222@notmail.com>
2022-11-19 05:39:05 +0500

    first commit

---
+++ src/test/java/ru/mefodovsky/testrest2dbh2/TestRest2dbH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mefodovsky.testrest2dbh2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRest2dbH2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
